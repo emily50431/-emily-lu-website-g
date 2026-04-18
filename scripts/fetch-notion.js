d31129c87598b23489fa63ad137c4983e2622ca7
emily50431 <emily50431@gmail.com>
2026-04-19 01:20:41 +0800

    改自我介紹

--- scripts/fetch-notion.js
+++ scripts/fetch-notion.js
@@ -841,7 +841,7 @@ function generateAboutHtml() {
       </div>
       <div class="facts-body">
         <div class="fact"><div class="fact-num">01</div><div class="fact-text">是一介即將邁入30歲的巨蟹座女子，坐標臺灣北部人</div></div>
-        <div class="fact"><div class="fact-num">02</div><div class="fact-text">MBTI 是 ESFP 表演者，對環境自在可以越表現自己</div></div>
+        <div class="fact"><div class="fact-num">02</div><div class="fact-text">MBTI 是 ESFP 表演者，對環境越自在，越可以好好表現自己</div></div>
         <div class="fact"><div class="fact-num">03</div><div class="fact-text">大學、研究所都唸測量相關，但本人不耐曬，默默選辦公室工作</div></div>
         <div class="fact"><div class="fact-num">04</div><div class="fact-text">以前是B2B Sales，現在是Presales，資訊軟體業打滾約 5 年</div></div>
         <div class="fact"><div class="fact-num">05</div><div class="fact-text">積極嘗試各種生活，近期著迷上皮克敏、壁球、看電子書</div></div>
